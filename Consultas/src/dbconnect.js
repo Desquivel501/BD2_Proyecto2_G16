61b524021e875b8c2442cea2c39db8cae9e67178
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2023-10-28 01:11:38 -0600

    Eliminado dbconnect

--- Consultas/src/dbconnect.js
+++
@@ -1,8 +0,0 @@
-
-import { MongoClient } from 'mongodb'
-import dotenv from 'dotenv'
-dotenv.config()
-
-var url = process.env.MONGO_HOST
-
-export const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
